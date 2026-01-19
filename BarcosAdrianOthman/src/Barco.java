7fa1568dfefa11e2d293f158cb527a143ff2e120
Xoth-manX <1DAM@A11-PC12>
2026-01-19 11:58:42 +0100

    barco

--- BarcosAdrianOthman/src/Barco.java
+++ BarcosAdrianOthman/src/Barco.java
@@ -1,9 +1,37 @@
 
-public class Barco1 {
+public class Barco {
+protected String modelo;
+protected int potencia;
+protected double capacidadCombustible;
+public Barco(String modelo, int potencia, double capacidadCombustible) {
+	super();
+	this.modelo = modelo;
+	this.potencia = potencia;
+	this.capacidadCombustible = capacidadCombustible;
+}
+public String getModelo() {
+	return modelo;
+}
+public void setModelo(String modelo) {
+	this.modelo = modelo;
+}
+public int getPotencia() {
+	return potencia;
+}
+public void setPotencia(int potencia) {
+	this.potencia = potencia;
+}
+public double getCapacidadCombustible() {
+	return capacidadCombustible;
+}
+public void setCapacidadCombustible(double capacidadCombustible) {
+	this.capacidadCombustible = capacidadCombustible;
+}
+@Override
+public String toString() {
+	return "Barco [modelo=" + modelo + ", potencia=" + potencia + ", capacidadCombustible=" + capacidadCombustible
+			+ "]";
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
